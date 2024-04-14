9efe547b1e8bf1077f62a6398f958250a1281f2a
Tanveer <shaikhtanveer5205@gmail.com>
2024-04-15 03:00:31 +0530

    user model

---
+++ src/models/user.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+import { type } from "os";
+
+const userSchema = new mongoose.Schema({
+  // MongoDB Schema for User Model
+  userName: {
+    type: String,
+    required: [true, "Provide a username"],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Provide a email"],
+    unique: true,
+    LowerCase: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Provide a password"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  forgotPasswordToken: String,
+  forgotPasswordTokenExpiry: Date,
+  verifyToken: String,
+  verifyTokenExpiry: Date,
+});
+
+// Nextjs working on edge computing so nextjs don't know it is already created or taking reference of a schema so we need to export the model like this.
+const User = mongoose.models.users || mongoose.model("users", userSchema);
+
+export default User;
