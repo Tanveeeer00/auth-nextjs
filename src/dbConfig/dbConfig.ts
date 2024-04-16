6137d751fbc815ec1a5f2b85f56a231b0c7a28ba
Tanveer <shaikhtanveer5205@gmail.com>
2024-04-16 23:17:36 +0530

    database connection

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+export async function connectToDB() {
+  try {
+    mongoose.connect(process.env.MONGO_URI as string);
+    const connection = mongoose.connection;
+
+    connection.on("connected", () => {
+      console.log("mongoDB connected");
+    });
+    connection.on("error", (err) => {
+      console.log("mongoDB connection failed: " + err);
+    });
+    process.exit(1);
+  } catch (error) {
+    console.log("Error connecting to DB", error);
+  }
+}
