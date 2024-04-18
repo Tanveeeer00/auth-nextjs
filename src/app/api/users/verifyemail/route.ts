d02b22b27cfd6bfcf47f666460adcec9940c3c17
Tanveer <shaikhtanveer5205@gmail.com>
2024-04-19 04:44:55 +0530

    verify email route

---
+++ src/app/api/users/verifyemail/route.ts
@@ -0,0 +1,32 @@
+import { connectToDB } from "@/dbConfig/dbConfig";
+import User from "@/models/user.model";
+import { NextRequest, NextResponse } from "next/server";
+
+connectToDB();
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { token } = reqBody;
+    console.log(reqBody, token);
+    const user = await User.findOne({
+      verifyToken: token,
+      verifyTokenExpiry: { $gt: Date.now() },
+    });
+    if (!user) {
+      return NextResponse.json({ error: "Invalid token" }, { status: 401 });
+    }
+    console.log(user);
+    user.isVerified = true;
+    user.verifyTokenExpiry = undefined;
+    user.verifyToken = undefined;
+
+    await user.save();
+
+    return NextResponse.json(
+      { message: "Email Verified successfully!", success: true },
+      { status: 201 }
+    );
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
